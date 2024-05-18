84b6d57ae89aabbb56069486a3ca5b0495110bf6
minakshi2330 <minut9056@gmail.com>
2024-05-18 15:21:42 +0530

    18-05-2024

--- youtude_rails/student/app/controllers/courses_controller.rb
+++ youtude_rails/student/app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
 	 before_action :set_course, only: [:edit , :update, :show , :destroy]
 
 	def index
-		@courses = Courses.all
+		@courses = Course.all
 	end
 
     def new 
@@ -10,12 +10,44 @@ class CoursesController < ApplicationController
     end
 
     def create
-    	@course = Course.create
+    	@course = Course.create(course_params)
+    	if @course.save
+   		redirect_to courses_path 
+   	else
+   		render 'new'
+   	end
     end
+
+    def edit
+    end
+
+    def update
+    	if @course.update(course_params)
+    	redirect_to courses_path, notice: 'course has been updated successfully'
+    else
+    	render :edit
+    end
+    end
+     
+    def show
+   	
+    end
+   
+    def destroy
+      if @course.destroy
+               redirect_to courses_path 
+            
+            end
+    end
+
+ private
+   def course_params
+    	params.require(:course).permit(:c_name, :student_id)
+   end
 
 	def set_course
 		@course = Course.find(params[:id])
 		rescue ActiveRecord::RecordNotFound => error
-        redirect_to students_path, notice: error
+        redirect_to courses_path, notice: error
     end
 end
--- youtude_rails/student/app/models/course.rb
+++ youtude_rails/student/app/models/course.rb
@@ -1,3 +1,4 @@
 class Course < ApplicationRecord
   belongs_to :student
+  validates :c_name, presence: true
 end
